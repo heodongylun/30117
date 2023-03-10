227c5d92ecfb5dd894b87b7f80c20a2af67d1925
Administrator <Administrator@sw303_13>
2023-03-10 11:06:55 +0900

    0310 프로젝트 추가

---
+++ 0309/src/TriangleExam.java
@@ -0,0 +1,10 @@
+
+public class TriangleExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Triangle tr=new Triangle(10.2, 17.3);
+		System.out.println("삼각형의 넓이");
+	}
+
+}
