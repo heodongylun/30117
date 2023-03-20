e2aa952ff13256512eac6fbbb09b9177ecb36f4c
허동륜 <112070243+heodongylun@users.noreply.github.com>
2023-03-20 15:53:18 +0900

    dd
    dd

---
+++ ArrayTest.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		
+		String[] names = new String[5];
+		for(int i=0; i<names.length; i++){
+	           System.out.print("이름을 입력하세요. ");
+	            names[i] = sc.next();            
+
+	        
+	           }
+	
+	}
+
+}
